abbb3570a49914a8db5fba9a21e1cfefb759080f
Chunhui Mo <kevinmoch@qq.com>
2024-05-20 17:40:54 +0800

    Create proxyData2.js

---
+++ vue/proxyData2.js
@@ -0,0 +1,26 @@
+const proxyData = function() {
+  const self = this;
+
+  Object.keys(this._data).forEach((key) => {
+    Object.defineProperty(self, key, {
+      get: () => self._data[key],
+      set(newValue) {
+        if (typeof self.$options.beforeUpdate === 'function') {
+          self.$options.beforeUpdate.call(self);
+        }
+
+        self._data[key] = newValue;
+        self._render();
+
+        if (typeof self.$options.updated === 'function') {
+          self.$options.updated.call(self);
+        }
+      }
+    });
+  });
+
+  const methods = this.$options.methods ?? {};
+  Object.keys(methods).forEach((key) => (self[key] = methods[key].bind(self)));
+};
+
+export { proxyData };
