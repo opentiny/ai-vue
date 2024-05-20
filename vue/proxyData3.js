5ae05c3c8a90f98e1b2a6343e7f54ad29d960fa6
Chunhui Mo <kevinmoch@qq.com>
2024-05-20 17:41:13 +0800

    Create proxyData3.js

---
+++ vue/proxyData3.js
@@ -0,0 +1,53 @@
+import { setCurrentInstance, getCurrentInstance } from './instance.js';
+
+const proxyData = function() {
+  const self = this;
+
+  Object.keys(this._data).forEach((key) => {
+    Object.defineProperty(self, key, {
+      get() {
+        const value = self._data[key];
+        setCurrentInstance(self);
+        return value?.instance === self ? value.callback() : value;
+      },
+      set(newValue) {
+        if (self._data[key]?.instance !== self) {
+          self._data[key] = newValue;
+        }
+      }
+    });
+  });
+};
+
+const reactive = (obj) => {
+  return new Proxy(obj, {
+    get(target, key) {
+      const instance = getCurrentInstance();
+      if (typeof instance._watchCallback === 'function') {
+        instance._eventBus.on(target, key, instance._watchCallback);
+      }
+      return target[key];
+    },
+    set(target, key, value) {
+      const instance = getCurrentInstance();
+
+      if (typeof instance.$options.beforeUpdate === 'function') {
+        instance.$options.beforeUpdate.call(instance);
+      }
+
+      const oldValue = target[key];
+      target[key] = value;
+
+      instance._eventBus.emit(target, key, [value, oldValue]);
+      instance._render();
+
+      if (typeof instance.$options.updated === 'function') {
+        instance.$options.updated.call(instance);
+      }
+
+      return true;
+    }
+  });
+};
+
+export { proxyData, reactive };
