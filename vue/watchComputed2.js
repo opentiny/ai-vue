edd8b71609a36b3c16991a05685ea72efd7ae420
Chunhui Mo <kevinmoch@qq.com>
2024-05-20 17:42:23 +0800

    Create watchComputed2.js

---
+++ vue/watchComputed2.js
@@ -0,0 +1,30 @@
+const watch = function() {
+  const self = this;
+  const watch = this.$options.watch || {};
+
+  Object.keys(watch).forEach((key) => {
+    const callback = watch[key];
+    let value = self._data[key];
+
+    Object.defineProperty(self._data, key, {
+      get: () => value,
+      set(newValue) {
+        callback.call(self, newValue, value);
+        value = newValue;
+      }
+    });
+  });
+};
+
+const computed = function() {
+  const self = this;
+  const computed = this.$options.computed ?? {};
+
+  Object.keys(computed).forEach((key) => {
+    Object.defineProperty(self, key, {
+      get: () => computed[key].call(self)
+    });
+  });
+};
+
+export { watch, computed };
